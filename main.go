f8c248ba816cddca7cc09d4f6834be47fdeb5ec6
Scott Redig <laremere@gmail.com>
2017-09-04 02:20:17 +0000

    Basic echo webserver

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    _, err := fmt.Fprintf(w, "Welcome to home.redig.us")
+    if (err != nil) {
+        log.Fatal(err)
+    }
+}
+
+func main() {
+    log.Println("Starting server")
+    http.HandleFunc("/", handler)
+    err := http.ListenAndServe(":80", nil)
+    if (err != nil) {
+        log.Fatal(err)
+    }
+}
